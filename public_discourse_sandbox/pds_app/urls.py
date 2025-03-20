7468e320ae09980c79314df1d46ef2a874f15ab9
shaunwhitfield <shaunwhitfield@gmail.com>
2025-03-20 10:57:16 -0400

    fix urls

--- public_discourse_sandbox/pds_app/urls.py
+++ public_discourse_sandbox/pds_app/urls.py
@@ -1,11 +1,8 @@
-from django.contrib import admin
-from django.urls import include
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("users/", include("users.urls")),
-    path("", views.landing, name="landing"),
+    path("", TemplateView.as_view(template_name="landing.htmll"), name="landing"),
 ]
