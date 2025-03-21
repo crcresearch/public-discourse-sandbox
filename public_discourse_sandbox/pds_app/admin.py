41b52719586f77b660f86faf5595979a00f430db
shaunwhitfield <shaunwhitfield@gmail.com>
2025-03-21 16:41:22 -0400

    Models, migration, admin

--- public_discourse_sandbox/pds_app/admin.py
+++ public_discourse_sandbox/pds_app/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
+from .models import Experiment, UserProfile, Post, Vote, SocialNetwork
 
-# Register your models here.
+@admin.register(Experiment)
+class ExperimentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'creator', 'created_date', 'last_modified')
+    search_fields = ('name', 'description')
+    list_filter = ('creator',)
+    readonly_fields = ('created_date', 'last_modified')
+    raw_id_fields = ('creator',)  # Uses a popup for better performance with many users
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'experiment', 'is_bot', 'is_private', 'is_deleted', 'num_followers', 'num_following')
+    search_fields = ('user__username', 'bio')
+    list_filter = ('is_bot', 'is_private', 'is_deleted', 'experiment')
+    readonly_fields = ('created_date', 'last_modified', 'num_followers', 'num_following')
+    raw_id_fields = ('user', 'experiment')
+    date_hierarchy = 'created_date'
+    ordering = ('-created_date',)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('user_profile', 'content_preview', 'experiment', 'num_upvotes', 'num_downvotes', 'created_date', 'is_deleted')
+    search_fields = ('content', 'user_profile__user__username')
+    list_filter = ('is_deleted', 'is_edited', 'is_pinned', 'experiment', 'created_date')
+    readonly_fields = ('created_date', 'last_modified', 'num_upvotes', 'num_downvotes', 'num_comments', 'num_shares')
+    raw_id_fields = ('user_profile', 'experiment', 'parent_post')
+    date_hierarchy = 'created_date'
+    ordering = ('-created_date',)
+    
+    def content_preview(self, obj):
+        return obj.content[:50] + "..." if len(obj.content) > 50 else obj.content
+    content_preview.short_description = 'Content Preview'
+
+@admin.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ('user_profile', 'post', 'is_upvote', 'created_date')
+    list_filter = ('is_upvote', 'created_date')
+    search_fields = ('user_profile__user__username',)
+    readonly_fields = ('created_date', 'last_modified')
+    raw_id_fields = ('user_profile', 'post')
+    date_hierarchy = 'created_date'
+    ordering = ('-created_date',)
+
+@admin.register(SocialNetwork)
+class SocialNetworkAdmin(admin.ModelAdmin):
+    list_display = ('source_node', 'target_node', 'created_date')
+    search_fields = ('source_node__user__username', 'target_node__user__username')
+    readonly_fields = ('created_date', 'last_modified')
+    raw_id_fields = ('source_node', 'target_node')
+    date_hierarchy = 'created_date'
+    ordering = ('-created_date',)
