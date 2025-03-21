9b22e58dfaec3638d937bbc0176930fb99184846
shaunwhitfield <shaunwhitfield@gmail.com>
2025-03-21 10:25:32 -0400

    restructure template directories

--- public_discourse_sandbox/pds_app/urls.py
+++ public_discourse_sandbox/pds_app/urls.py
@@ -4,6 +4,8 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="landing.html"), name="landing"),
-    path("home/", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("", TemplateView.as_view(template_name="pages/landing.html"), name="landing"),
+    path("about/", TemplateView.as_view(template_name="pages/about.html"), name="about"),
+    path("home/", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    path("explore/", TemplateView.as_view(template_name="pages/explore.html"), name="explore"),
 ]
