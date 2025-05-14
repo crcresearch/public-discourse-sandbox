71b0843d4fccabb0f1a61c7b4cc93eb3533e7085
shaunwhitfield <shaunwhitfield@gmail.com>
2025-05-14 17:39:37 -0400

    some cleanup

--- public_discourse_sandbox/pds_app/views.py
+++ public_discourse_sandbox/pds_app/views.py
@@ -15,7 +15,6 @@ from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.contrib.sites.shortcuts import get_current_site
 User = get_user_model()
 import json
 
